4e716d3692cd6f81db3b17643f84d38ce1b54649
MarcoRodriguez04 <marco_1322134127@uptecamac.edu.mx>
2024-08-06 14:01:13 -0600

    feat: Update letters array in ScrapingService

--- src/scraping/scraping.service.ts
+++ src/scraping/scraping.service.ts
@@ -25,7 +25,9 @@ export class ScrapingService {
     await page.waitForSelector('#rsoc');
     console.log('Página redireccionada');
 
-    let letters = ['a', 'b'];
+    let letters = ['a', 'b',"c", "d", "e", "f", "g", "h", "i", "j",
+      "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t",
+      "u", "v", "w", "x", "y", "z"];
     for (let i = 0; i < letters.length; i++) {
       for (let j = 0; j < letters.length; j++) {
         for (let k = 0; k < letters.length; k++) {
